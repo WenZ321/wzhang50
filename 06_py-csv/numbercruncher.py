bb36ea4c7f4f2c38bb32a732a58ec3d3fcfc5858
Wen Zhang <wenzhang.a12@gmail.com>
2024-09-20 11:47:26 -0400

    9/20 cw

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,7 +1,7 @@
 #Wen Zhang
 #China Rats plus One
 #Soft Dev
-#<Processing a csv file(python dictionaries) and using weight percentages to select a random job>
+#<Processing a csv file(python dictionaries) and using weighted percentages to select a random job>
 #2024-09-19
 #Time spent: ~30 minutes
 
@@ -21,8 +21,10 @@ occupation_list = occupation_list[1:-1]
 percentages = percentages[1:-1]
 
 def select_occupation(occupation_list, percentages):
-    selected = random.choices(occupation_list, weights=percentages, k=1)
-    return selected[0]
+    return random.choices(occupation_list, weights=percentages, k=1)
+     
 
 for i in range(100):
-        print(select_occupation(occupation_list, percentages))
+    print(select_occupation(occupation_list, percentages))
+        
+        
