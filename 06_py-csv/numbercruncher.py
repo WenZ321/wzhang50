5cc285586d765e26c34aff1a4f3859501901994e
Wen Zhang <wzhang50@stuy.edu>
2024-09-19 20:26:49 -0400

    Finished making the selected weight function

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,11 +1,28 @@
+#Wen Zhang
+#China Rats plus One
+#Soft Dev
+#<Processing a csv file(python dictionaries) and using weight percentages to select a random job>
+#2024-09-19
+#Time spent: ~30 minutes
+
 import random
 import csv
 
 occupation_list = []
+percentages = []
 
 with open('occupations.csv', newline ='') as csvfile:
     file = csv.DictReader(csvfile)
     for row in file:
-        occupation_list.append(row)
+        occupation_list.append(row['Job Class'])
+        percentages.append((float)(row['Percentage']))
 
-print(occupation_list)
+occupation_list = occupation_list[1:-1]
+percentages = percentages[1:-1]
+
+def select_occupation(occupation_list, percentages):
+    selected = random.choices(occupation_list, weights=percentages, k=1)
+    return selected[0]
+
+for i in range(100):
+        print(select_occupation(occupation_list, percentages))
