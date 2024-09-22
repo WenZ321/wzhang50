7bd209fc81d75f681b03c690969c8ad0f411a50e
Wen Zhang <wzhang50@stuy.edu>
2024-09-22 15:02:47 -0400

    answered questions for teardown'

--- 08_teardown/app.py
+++ 08_teardown/app.py
@@ -3,14 +3,16 @@
 '''
 DISCO:
 <note any discoveries you made here... no matter how small!>
+If I copy and paste the numbers of the servers into a browser, the website will have "NO Hablo Espanol"
+When I changed what is in the return, the website also matches to what I changed
 
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0. In java, where we would create an object from a class. In this example, a Flask class.
+1. This could be like going to the homepage
+2. I think this will print to the console/terminal where the Flask server is running
+3. I am not sure
+4. I think it will appear on the website where the flask server is being runnmed
+5. I have seen this in java, where we call a function of a class through an object of that class
  ...
 
 INVESTIGATIVE APPROACH:
