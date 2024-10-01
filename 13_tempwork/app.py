95fb4b1475c12c792c7133471a2d566b15fef3d7
Wen Zhang <wzhang50@stuy.edu>
2024-09-30 21:54:25 -0400

    almost finished with the hw

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -11,28 +11,35 @@ import random
 import csv
 
 occupation_dict = []
+occupation_list = []
+percentages = []
 
-with open('occupations.csv', newline ='') as csvfile:
+with open('data/occupations.csv', newline ='') as csvfile:
     file = csv.DictReader(csvfile)
-    occupation_dict = file
+    for row in file:
+        occupation_list.append(row['Job Class'])
+        percentages.append(float(row['Percentage']))
+        occupation_dict.append([row['Job Class'], float(row['Percentage'])])
+        
 
-occupation_dict = occupation_dict[1:-1]
+occupation_list = occupation_dict[0:-1]
+occupation_list = occupation_list[1:-1]
+percentages = percentages[1:-1]
 
 def select_occupation(occupation_list, percentages):
     return random.choices(occupation_list, weights=percentages, k=1)
      
     # -------------------------------------------------------------
 
-from flask import Flask
+
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/wdywtbwygp")
 def page():
     occupation = select_occupation(occupation_list, percentages)
-    
-    
-    
-    return html
+    return render_template('tablified.html', title = "Job Table", Job = occupation[0], Occ = "Jobs", collection = occupation_dict)
     
     
 if __name__ == "__main__":      
