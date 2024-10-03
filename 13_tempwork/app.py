14a1b2165e6c85d339d43e8fab00682d574fb953
Wen Zhang <wzhang50@stuy.edu>
2024-10-03 10:40:30 -0400

    updated with newest work

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -6,28 +6,30 @@ K13 -- creating template to make html table
 2024-09-30
 Time spent: 30 mins
 '''
-# https://www.bls.gov/ooh/management/
+
 import random
 import csv
 
 occupation_dict = []
 occupation_list = []
 percentages = []
+links=[]
 
-with open('data/occupations.csv', newline ='') as csvfile:
+with open('data/occupations.csv', newline ='') as csvfile:  #Reading csv and assigning values to their respective lists
     file = csv.DictReader(csvfile)
     for row in file:
         occupation_list.append(row['Job Class'])
         percentages.append(float(row['Percentage']))
-        occupation_dict.append([row['Job Class'], float(row['Percentage'])])
+        links.append(row['Helpful Link'])
+        occupation_dict.append([row['Job Class'], float(row['Percentage']),row["Helpful Link"]]) 
         
 
-occupation_dict = occupation_dict[0:-1]
+occupation_dict = occupation_dict[0:-1]  #Splicing lists and dictionaries
 occupation_list = occupation_list[1:-1]
 percentages = percentages[1:-1]
 
 def select_occupation(occupation_list, percentages):
-    return random.choices(occupation_list, weights=percentages, k=1)
+    return random.choices(occupation_list, weights=percentages, k=1)   #Randomly selecting occupation based on percentages
      
     # -------------------------------------------------------------
 
@@ -36,7 +38,7 @@ def select_occupation(occupation_list, percentages):
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/wdywtbwygp")
+@app.route("/wdywtbwygp")      #Flask and jinja using template
 def page():
     head = "A table of job occupation with helpful links to help you get started with that guide"
     tnpgroster = "Made In Africa - Wen, Jackie, Danny"
