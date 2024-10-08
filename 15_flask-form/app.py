7a0b8b3d8d20038202b280385bed72591c8a003c
Wen Zhang <wenzhang.a12@gmail.com>
2024-10-08 11:43:49 -0400

    login page now passes information to response page

---
+++ 15_flask-form/app.py
@@ -0,0 +1,59 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# October 2024
+
+# import conventions:
+# list most general first (standard python library)
+# ...then pip installs (eg Flask)
+# ...then your own home-rolled modules/packages (today's test module)
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    ### All of these print statements print to the terminal
+    
+    #print(testmod0.goo())
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")
+    #print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/response.html") # , methods=['GET', 'POST'])
+def responsePage():
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")
+    #print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    return render_template( 'response.html', username = request.args['username']) #response to a form submission
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
