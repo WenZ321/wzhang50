8f06d3898d3365085a0f6eb635b4da93f0603ec6
Wen Zhang <wenzhang.a12@gmail.com>
2025-01-06 11:47:34 -0500

    fact cw:

---
+++ 27_js/fibfac.js
@@ -0,0 +1,38 @@
+//
+//SoftDev pd0
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+
+//<your team's fact(n) implementation>
+
+function fact(n){
+  if (n == 1){
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+} 
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+
+
+
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+//=================================================================
