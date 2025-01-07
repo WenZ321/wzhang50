b4be8608c20f82304f19982b9cb5dd7f72338c69
Wen Zhang <wzhang50@stuy.edu>
2025-01-06 23:15:07 -0500

    finished 27 hw

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,13 +1,14 @@
-//
-//SoftDev pd0
+//Team WL :: Wen Zhang, Christopher Louie
+//SoftDev pd 5
 //K27 - Basic functions in JavaScript
 //2025-01-06m
+//Time Spent: 30 mins
 
 //JavaScript implementations of Day0 recursive Scheme functions
 
 //factorial:
 
-//<your team's fact(n) implementation>
+
 
 function fact(n){
   if (n == 1){
@@ -19,14 +20,27 @@ function fact(n){
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+console.log(fact(1)); // "...should be 1"
+console.log(fact(2)); // "...should be 2"
+console.log(fact(3)); // "...should be 6"
+console.log(fact(4)); // "...should be 24"
+console.log(fact(5)); // "...should be 120"
 
 //-----------------------------------------------------------------
 
 
 //fib:
 
-//<your team's fib(n) implementation>
+let fib = function(n){
+  if (n ==0){
+    return 0;
+  }
+    else if (n==1){
+        return 1;}
+    else {
+    return (fib(n-1)+fib(n-2));
+  }
+}
 
 
 
@@ -34,5 +48,12 @@ function fact(n){
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+console.log(fib(0)); // "...should be 0"
+console.log(fib(1)); // "...should be 1"
+console.log(fib(2)); // "...should be 1"
+console.log(fib(3)); // "...should be 2"
+console.log(fib(4)); // "...should be 3"
+console.log(fib(5)); // "...should be 5"
 
 //=================================================================
